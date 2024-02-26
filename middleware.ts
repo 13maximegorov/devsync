7d551a1992677fe1f0a0a1fc907b5d68e876b150
Maxim Egorov <13maximegorov@gmail.com>
2024-02-26 10:54:59 +0500

    fix

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,7 @@ import NextAuth from 'next-auth';
 
 const { auth } = NextAuth(authConfig);
 
+// @ts-ignore
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
